d3d257d47b6b3d004b2b8501b211ef5d97c194ed
hkicin1 <hkicin1@etf.unsa.ba>
2020-05-01 19:03:49 +0200

    Implementirana klasa Malfunction -- dodani atributi, getteri, setteri i konstruktori

--- src/autokuca/Malfunction.java
+++ src/autokuca/Malfunction.java
@@ -1,4 +1,42 @@
 package autokuca;
 
+import java.time.LocalDate;
+
 public class Malfunction {
+    private String malfunctionName;
+    private LocalDate emergenceDate;
+    private LocalDate repairDate;
+
+    public Malfunction() {
+    }
+
+    public Malfunction(String malfunctionName, LocalDate emergenceDate, LocalDate repairDate) {
+        this.malfunctionName = malfunctionName;
+        this.emergenceDate = emergenceDate;
+        this.repairDate = repairDate;
+    }
+
+    public String getMalfunctionName() {
+        return malfunctionName;
+    }
+
+    public void setMalfunctionName(String malfunctionName) {
+        this.malfunctionName = malfunctionName;
+    }
+
+    public LocalDate getEmergenceDate() {
+        return emergenceDate;
+    }
+
+    public void setEmergenceDate(LocalDate emergenceDate) {
+        this.emergenceDate = emergenceDate;
+    }
+
+    public LocalDate getRepairDate() {
+        return repairDate;
+    }
+
+    public void setRepairDate(LocalDate repairDate) {
+        this.repairDate = repairDate;
+    }
 }
