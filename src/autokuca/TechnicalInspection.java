e4a30f0c4e25ca7db474efb29702e505a9d22ba4
hkicin1 <hkicin1@etf.unsa.ba>
2020-05-01 18:56:07 +0200

    Kreirana klasa TechnicalInspection, sa getterima i setterima Enumi i klasa Vehicle samo kreirani, bez implementacije

---
+++ src/autokuca/TechnicalInspection.java
@@ -0,0 +1,52 @@
+package autokuca;
+
+import Enum.*;
+
+public class TechnicalInspection {
+    private InspectionType inspectionType;
+    private User responsibleEmployee;
+    private Vehicle vehicle;
+    private WarrantState warrantState;
+
+    public TechnicalInspection() {
+    }
+
+    public TechnicalInspection(InspectionType inspectionType, User responsibleEmployee, Vehicle vehicle, WarrantState warrantState) {
+        this.inspectionType = inspectionType;
+        this.responsibleEmployee = responsibleEmployee;
+        this.vehicle = vehicle;
+        this.warrantState = warrantState;
+    }
+
+    public InspectionType getInspectionType() {
+        return inspectionType;
+    }
+
+    public void setInspectionType(InspectionType inspectionType) {
+        this.inspectionType = inspectionType;
+    }
+
+    public User getResponsibleEmployee() {
+        return responsibleEmployee;
+    }
+
+    public void setResponsibleEmployee(User responsibleEmployee) {
+        this.responsibleEmployee = responsibleEmployee;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public WarrantState getWarrantState() {
+        return warrantState;
+    }
+
+    public void setWarrantState(WarrantState warrantState) {
+        this.warrantState = warrantState;
+    }
+}
