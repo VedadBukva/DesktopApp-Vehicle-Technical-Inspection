6badb0c10d267755497eecd60ef9d14efe53ff00
hkicin1 <hkicin1@etf.unsa.ba>
2020-05-08 20:02:48 +0200

    Dodana metoda za dohvatanje stanja naloga

--- src/Enum/WarrantState.java
+++ src/Enum/WarrantState.java
@@ -6,4 +6,18 @@ public enum WarrantState {
     WarrantState() {
     }
 
+    public static WarrantState getWarrantState(String type) {
+        switch (type) {
+            case "NA PREGLEDU": return IN_PROGRESS;
+            case "ZAVRSEN": return DONE;
+            case "NA CEKANJU": return ON_HOLD;
+            case "U ARHIVI": return IN_ARCHIVE;
+            case "ON_HOLD": return ON_HOLD;
+            case "IN PROGRESS": return IN_PROGRESS;
+            case "DONE": return DONE;
+            case "IN_ARCHIVE": return IN_ARCHIVE;
+        }
+        return null;
+    }
+
 }
