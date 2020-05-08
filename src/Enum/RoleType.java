cfa98da2427b2ba398cb7eb0cb15516da8e0df8c
hkicin1 <hkicin1@etf.unsa.ba>
2020-05-09 01:09:36 +0200

    Dodana enum klasa za tip uloge

--- src/Enum/RoleType.java
+++ src/Enum/RoleType.java
@@ -1,4 +1,20 @@
 package Enum;
 
 public enum RoleType {
+    EMPLOYEE, MENAGER, ADMIN;
+
+    RoleType() {
+    }
+
+    public static RoleType getRoleType(String type) {
+        switch (type) {
+            case "EMPLOYEE": return EMPLOYEE;
+            case "MENAGER": return MENAGER;
+            case "ADMIN": return ADMIN;
+            case "RADNIK": return EMPLOYEE;
+            case "MENADZER": return MENAGER;
+            case "ADMINISTRATOR": return ADMIN;
+        }
+        return null;
+    }
 }
